d9f61965c886e95c5f32de529dd8b5e1f1dc4689
MaicolAvila <maicol.2001@hotmail.com>
2021-12-20 17:54:15 -0500

    Projects ready to api

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,18 +5,36 @@ import CardProject from "../../components/CardProject/CardProject";
 import "./Projects.scss";
 
 export default class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: "MOTION CMS",
+          description: `CMS desarrollado en reactJs, con un manejo de datos desde el erp
+          odoo y backend en python.`,
+          image: cmsImage,
+          link_vew: "",
+          link_know: "",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className="Projects">
         <div className="Projects-container">
-          <CardProject
-            image={cmsImage}
-            link_vew=""
-            link_know=""
-            title="MOTION CMS"
-            description="CMS desarrollado en reactJs, con un manejo de datos desde el erp
-            odoo y backend en python."
-          ></CardProject>
+          {this.state.projects.map((project) => {
+            return (
+              <CardProject
+                title={project.title}
+                description={project.description}
+                image={project.image}
+                link_vew={project.link_vew}
+                link_know={project.link_know}
+              />
+            );
+          })}
         </div>
       </div>
     );
